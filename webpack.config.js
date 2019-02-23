4d74acff3211c05a6bb00ed3c9c66c2ee4322bef
Mikhail Podanev <mpodanev@MacBook-Pro-Mikhail.local>
2019-02-23 20:40:26 +0200

    completed task

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`, // Режим сборки
+  entry: `./src/main.js`, // Точка входа приложения
+  output: { // Настройка выходного файла
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devtool: `source-map`,
+  devServer: {
+    contentBase: path.join(__dirname, `public`), // Где искать сборку
+    publicPath: `http: //localhost:8080/`, // Веб адрес сборки
+    hot: true, // Автоматическая перезагрузка страницы
+    compress: true // Сжатие
+  }
+};
